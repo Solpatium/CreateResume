860b2c58489a6afcd05a99e06d0daad7980fe58d
Daniel Kłódka <danielklodka@gmail.com>
2018-06-12 22:04:39 +0200

    Add implementation for preview in WorkField

--- client/src/fields/WorkField.tsx
+++ client/src/fields/WorkField.tsx
@@ -14,6 +14,10 @@ interface IWorkFieldProps extends IFieldProps {
     position?: string,
     description?: string,
     location?: string,
+    startYear?: number,
+    endYear?: number,
+    startMonth?: number,
+    endMonth?: number,
     currently?: boolean
 }
 
@@ -24,13 +28,21 @@ interface IWorkFieldState extends IFieldState {
     position: string,
     description: string,
     location: string,
+    startYear: number,
+    endYear: number,
+    startMonth: number,
+    endMonth: number,
     currently: boolean
 }
 
 export default class WorkField extends Field<IWorkFieldProps, IWorkFieldState> {
     constructor(props: IWorkFieldProps) {
         super(props)
-        const state: IWorkFieldState = {
+        this.state = {
+            startYear: props.startYear ? props.startYear : 0,
+            endYear: props.endYear ? props.endYear : 0,
+            startMonth: props.startMonth ? props.startMonth : 0,
+            endMonth: props.endMonth ? props.endMonth : 0,
             position: props.position ? props.position : '',
             description: props.description ? props.description : '',
             hidden: false,
@@ -39,16 +51,6 @@ export default class WorkField extends Field<IWorkFieldProps, IWorkFieldState> {
             company: props.company ? props.company : '',
             currently: props.currently ? props.currently : false,
         }
-
-        if( props.from ) {
-            state.from = props.from;
-        }
-
-        if( props.to ) {
-            state.to = props.to;
-        }
-
-        this.state = state;
     }
 // <Checkbox onChange={this.onChange}>Currrently working</Checkbox>
     public get name() { return 'Work experience' };
@@ -58,51 +60,82 @@ export default class WorkField extends Field<IWorkFieldProps, IWorkFieldState> {
             <Row>
                 <Col span={8}>
                     <FormItem label="Select start date">
-                        <MonthPicker placeholder="Start date"/>
+                        <MonthPicker placeholder="Start date" onChange={this.updateStartDate}/>
                     </FormItem>
                 </Col>
                 <Col span={8}>
                     <FormItem label="Select end date">
-                        <MonthPicker placeholder="End date" disabled={this.state.currently}/>
+                        <MonthPicker placeholder="End date" onChange={this.updateEndDate} disabled={this.state.currently}/>
                     </FormItem>
-                    <Switch onChange={this.notifyChange} />
+                    <Switch onChange={this.updateCurrently} />
                     <label>Currently working</label>
                 </Col>
                 <Col span={8}>
                 <FormItem label="Location">
-                    <Input placeholder="Kraków"/>
+                    <Input placeholder="Kraków" onChange={this.updateLocation} />
                 </FormItem>
                 </Col>
             </Row>
             <Row>
                 <Col span={12}>
                     <FormItem label="Company">
-                        <Input placeholder="Google" style={{ "width" : "90%"}}/>
+                        <Input placeholder="Google" onChange={this.updateCompany} style={{ "width" : "90%"}}/>
                     </FormItem>
                 </Col>
                 <Col span={12}>
                     <FormItem label="Position">
-                        <Input placeholder="Developer" />
+                        <Input placeholder="Developer" onChange={this.updatePosition}/>
                     </FormItem>
                 </Col>
             </Row>
             <Row>
                 <Col span={48}>
                     <FormItem label="Job Description">
-                        <TextArea placeholder="I was doing pancakes" onChange={this.update} rows={5}/>
+                        <TextArea placeholder="I was doing pancakes" onChange={this.updateDescription} rows={5}/>
                     </FormItem>
                 </Col>
             </Row>
             </div>)
     }
 
-    public onChange = (event: any) => {
-        this.setState({
-            currently: !this.state.currently,
-        });
+    public updateCompany= (event: React.ChangeEvent<HTMLInputElement>) => {
+        this.setState({company: event.target.value});
+        this.notifyChange()
+    }
+    public updatePosition= (event: React.ChangeEvent<HTMLInputElement>) => {
+        this.setState({position: event.target.value});
+        this.notifyChange()
+    }
+    public updateDescription= (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        this.setState({description: event.target.value});
+        this.notifyChange()
+    }
+    public updateEndDate = (date: moment.Moment, dateString: string) => {
+        if( date !== null) {
+            this.setState({endYear: date.year(), endMonth: date.month() + 1});
+        }
+        else{
+            this.setState({endYear: 0, endMonth: 0});
+        }
+        this.notifyChange()
+    }
+    public updateStartDate = (date: moment.Moment, dateString: string) => {
+        if( date !== null) {
+            this.setState({startYear: date.year(), startMonth: date.month() + 1});
+        }
+        else{
+            this.setState({startYear: 0, startMonth: 0});
+        }
+        this.notifyChange()
+    }
+    public updateCurrently= (event: any) => {
+        this.setState({currently: !this.state.currently })
+        this.notifyChange()
+    }
+    public updateLocation= (event: React.ChangeEvent<HTMLInputElement>) => {
+        this.setState({location: event.target.value});
         this.notifyChange()
     }
 
-    public update = (event: React.ChangeEvent<HTMLTextAreaElement>) => this.setState({description: event.target.value})
 
 }
